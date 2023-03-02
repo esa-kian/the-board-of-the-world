d98beaaea20d4c7170f04aefbc29d3fd9dce9b5d
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2023-03-02 23:56:24 +0200

    sum func and test

--- hello.go
+++ hello.go
@@ -1,14 +1,24 @@
 package main
 
 import "fmt"
+import "./iteration"
 
 const helloPrefix = "Hello, "
 
 func Hello(name string) string {
 
+	if name == "" {
+		name = "World"
+	}
+
 	return helloPrefix + name + "!"
 }
 
+func Add(x, y int) int {
+	return x + y
+}
+
 func main() {
+	fmt.Printls(iteration.Repeat())
 	fmt.Println(Hello("Ehsan and Zara"))
 }
--- hello_test.go
+++ hello_test.go
@@ -1,9 +1,10 @@
 package main
 
 import "testing"
+import "fmt"
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people", func(t *testing.T)) {
+	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Ehsan")
 		want := "Hello, Ehsan!"
 
@@ -11,4 +12,31 @@ func TestHello(t *testing.T) {
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
+
+	t.Run("say hello when an empty string is supplied", func(t *testing.T){
+		got := Hello("")
+		want := "Hello, World!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+
+}
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+	
+	fmt.Println("Answer is: ", sum)
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }

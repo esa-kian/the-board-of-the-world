8648a031145e2c288af5b8dfbd7d808e815521a9
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2023-03-15 00:31:51 +0200

    Dependency injection

--- hello.go
+++ hello.go
@@ -2,13 +2,57 @@ package main
 
 import "fmt"
 import "math"
+import "errors"
 
 const helloPrefix = "Hello, "
 
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	d[word] = definition
+	return nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
 
 type Bitcoin int
